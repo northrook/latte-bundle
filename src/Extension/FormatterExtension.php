f5c48f7d5cf74c5b0df473f1b306a004ae4047a2
Martin <hello@northrook.com>
2024-07-08 06:53:24 +0100

    mend

--- src/Extension/FormatterExtension.php
+++ src/Extension/FormatterExtension.php
@@ -3,18 +3,16 @@
 namespace Northrook\Latte\Extension;
 
 use Latte;
-use Northrook\ContentFormatter;
-use Northrook\Debug;
 use Northrook\ContentFormatter\Format;
 
 final class FormatterExtension extends Latte\Extension
 {
     public function getFilters() : array {
         return [
-            'nl2auto' => static fn ( $string ) => Format::nl2Auto( $string ),
-            'nl2p'    => static fn ( $string ) => Format::nl2p( $string ),
-            'nl2span' => static fn ( $string ) => Format::nl2span( $string ),
-            'formatInline' => [$this, 'formatInline'],
+            'nl2auto'      => static fn ( $string ) => Format::nl2Auto( $string ),
+            'nl2span'      => static fn ( $string ) => Format::nl2span( $string ),
+            'nl2p'         => static fn ( $string ) => Format::nl2p( $string ),
+            'formatInline' => [ $this, 'formatInline' ],
         ];
     }
 
